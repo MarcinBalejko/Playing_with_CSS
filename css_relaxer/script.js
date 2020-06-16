3aaaec064855e10e8c81ac7d2c845e9fd411150a
MarcinBalejko <mbbm9@o2.pl>
2020-06-16 12:11:54 +0200

    :recycle::sparkles: added relaxing soundtrack player

--- css_relaxer/script.js
+++ css_relaxer/script.js
@@ -1,5 +1,10 @@
 const container = document.getElementById("container");
 const text = document.querySelector("#text");
+const soundbtn = document.querySelector(".sound-button");
+const soundicon = document.getElementById("sound-icon");
+let audio = new Audio();
+audio.src = "./sound/track.mp3";
+audio.loop = true;
 
 const totalTime = 7500;
 const breatheTime = (totalTime / 5) * 2;
@@ -22,3 +27,15 @@ function breathAnimation() {
 }
 
 setInterval(breathAnimation, totalTime);
+
+function initAudioPlayer() {
+  if (soundicon.classList.contains("active")) {
+    soundicon.setAttribute("class", "fas fa-pause fa-2x sound notactive");
+    audio.play();
+  } else {
+    soundicon.setAttribute("class", "fas fa-music fa-2x sound active");
+    audio.pause();
+  }
+}
+
+soundbtn.addEventListener("click", initAudioPlayer);
