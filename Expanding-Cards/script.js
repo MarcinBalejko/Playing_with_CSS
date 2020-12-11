3556f56d0143fa66eddfefd4080a53f9d3459322
MarcinBalejko <mbbm9@o2.pl>
2020-12-11 01:16:51 +0100

    :recycle: Added loop slide show functionality

--- Expanding-Cards/script.js
+++ Expanding-Cards/script.js
@@ -12,3 +12,45 @@ function removeActiveClasses() {
     panel.classList.remove("active");
   });
 }
+
+function slideShow() {
+  const panel1 = panels[0];
+  const panel2 = panels[1];
+  const panel3 = panels[2];
+  const panel4 = panels[3];
+  const panel5 = panels[4];
+
+  setTimeout(() => {
+    removeActiveClasses();
+    panel1.classList.add("active");
+  }, 1000);
+
+  setTimeout(() => {
+    removeActiveClasses();
+    panel2.classList.add("active");
+  }, 7000);
+
+  setTimeout(() => {
+    removeActiveClasses();
+    panel3.classList.add("active");
+  }, 13000);
+
+  setTimeout(() => {
+    removeActiveClasses();
+    panel4.classList.add("active");
+  }, 19000);
+
+  setTimeout(() => {
+    removeActiveClasses();
+    panel5.classList.add("active");
+  }, 25000);
+}
+
+function continuousRun() {
+  slideShow();
+  setInterval(() => {
+    slideShow();
+  }, 31000);
+}
+
+continuousRun();
