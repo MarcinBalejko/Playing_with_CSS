ae902a979ce9eecb4108dc739b5479ec98fb863a
MarcinBalejko <mbbm9@o2.pl>
2020-12-11 01:51:30 +0100

    :pencil2: typos corrected

--- Expanding-Cards/script.js
+++ Expanding-Cards/script.js
@@ -14,35 +14,29 @@ function removeActiveClasses() {
 }
 
 function slideShow() {
-  const panel1 = panels[0];
-  const panel2 = panels[1];
-  const panel3 = panels[2];
-  const panel4 = panels[3];
-  const panel5 = panels[4];
-
   setTimeout(() => {
     removeActiveClasses();
-    panel1.classList.add("active");
+    panels[0].classList.add("active");
   }, 1000);
 
   setTimeout(() => {
     removeActiveClasses();
-    panel2.classList.add("active");
+    panels[1].classList.add("active");
   }, 7000);
 
   setTimeout(() => {
     removeActiveClasses();
-    panel3.classList.add("active");
+    panels[2].classList.add("active");
   }, 13000);
 
   setTimeout(() => {
     removeActiveClasses();
-    panel4.classList.add("active");
+    panels[3].classList.add("active");
   }, 19000);
 
   setTimeout(() => {
     removeActiveClasses();
-    panel5.classList.add("active");
+    panels[4].classList.add("active");
   }, 25000);
 }
 
