521ae2f123aac1a216bd954b0b4e2e1c7a756a91
MarcinBalejko <mbbm9@o2.pl>
2021-02-19 19:35:02 +0100

    :recycle: Functions refactored, typos corrected

--- drag-drop/script.js
+++ drag-drop/script.js
@@ -7,12 +7,11 @@ fill.addEventListener("dragend", dragEnd);
 for (const empty of empties) {
   empty.addEventListener("dragover", dragOver);
   empty.addEventListener("dragenter", dragEnter);
-  empty.addEventListener("dragoleave", dragLeave);
+  empty.addEventListener("dragleave", dragLeave);
   empty.addEventListener("drop", dragDrop);
 }
 
 function dragStart() {
-  console.log("drag start");
   this.className += " hold";
   setTimeout(() => {
     (this.className = "invisible"), 0;
@@ -20,22 +19,22 @@ function dragStart() {
 }
 
 function dragEnd() {
-  console.log("drag end");
   this.className = "fill";
 }
 
 function dragOver(e) {
   e.preventDefault();
-  // ^^^ dragover and dragenter have default actions
+  // ^ dragover and dragenter have default actions
   // so I want to prevent their default behavior here
 }
 
 function dragEnter(e) {
   e.preventDefault();
+  this.className += " hovered";
 }
 
 function dragLeave() {
-  console.log("drag leave");
+  this.className = "empty";
 }
 
 function dragDrop() {
