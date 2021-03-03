cc00a5e7db7b303298168016492cacd5e4e5948f
MarcinBalejko <mbbm9@o2.pl>
2021-03-03 01:53:05 +0100

    :recycle: minor html, css & js code changes

--- verify_account_UI/script.js
+++ verify_account_UI/script.js
@@ -7,7 +7,7 @@ codes.forEach((code, idx) => {
     if (e.key >= 0 && e.key <= 9) {
       codes[idx].value = "";
       setTimeout(() => codes[idx + 1].focus(), 10);
-    } else if (e.key === "Backspace") {
+    } else if (e.key == "Backspace") {
       setTimeout(() => codes[idx - 1].focus(), 10);
     }
   });
